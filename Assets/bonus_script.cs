031c38ae0b76c641a87a73115adc8bf8499e95c0
NightRise214 <jumpevo.1001@gmail.com>
2015-02-05 03:39:15 -0800

    hi

---
+++ Assets/bonus_script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bonus_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(transform.parent.gameObject.tag != "Bonus")
+		{
+			renderer.enabled = false;
+		}
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (renderer.enabled == true) {
+			Vector3 parentPos = transform.parent.transform.localPosition;
+			Vector3 parentScale = transform.parent.lossyScale;
+			Vector3 platform = new Vector3 (parentPos.x, parentPos.y + parentScale.y, parentPos.z);
+			transform.position = platform;
+		}
+	
+	}
+}
