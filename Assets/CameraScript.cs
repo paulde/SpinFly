d342b3a79ab368f5f1910ba2bb609d90446edf8e
NightRise214 <jumpevo.1001@gmail.com>
2015-02-23 23:05:52 -0800

    Camera Rotation

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -3,9 +3,21 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour {
 
+	private float x_pos;
+	private float z_pos;
+	GameObject playerObj;
+
 	// Use this for initialization
 	void Start () 
 	{
+		playerObj = GameObject.Find ("Player");
+
+		//Initiate starting position
+		x_pos = playerObj.transform.position.x;
+		z_pos = playerObj.transform.position.z - 5;
+		transform.position = new Vector3 (x_pos, playerObj.transform.position.y + 13, z_pos);
+		transform.LookAt (playerObj.transform.position);
+
 	
 	}
 	
@@ -13,7 +25,7 @@ public class CameraScript : MonoBehaviour {
 	void Update () 
 	{
 		//var v = new Vector3( 0, 
-		GameObject playerObj = GameObject.Find ("Player");
+
 		//var v = new Vector3 (playerObj.transform.position.x + playerObj.rigidbody.velocity.x, transform.position.y, playerObj.transform.position.z 
 		 //                    + playerObj.rigidbody.velocity.z );
 
@@ -21,6 +33,20 @@ public class CameraScript : MonoBehaviour {
 		var y = playerObj.transform.position.y + 13;
 		//if( y < 20 )
 	//		y = 20;
+
+
+		if (Input.GetKey (KeyCode.Q)) {
+			transform.RotateAround(playerObj.transform.position,Vector3.up, 40* Time.deltaTime);
+			x_pos = transform.position.x - playerObj.transform.position.x ;
+			z_pos = transform.position.z - playerObj.transform.position.z ;
+		}
+		if (Input.GetKey (KeyCode.E)) {
+			transform.RotateAround(playerObj.transform.position,Vector3.up, -40* Time.deltaTime);
+			x_pos = transform.position.x - playerObj.transform.position.x;
+			z_pos = transform.position.z - playerObj.transform.position.z;
+		}
+
+		
 		Vector3 v;
 
 		if(Input.GetButton("Fire1")) 
@@ -32,7 +58,7 @@ public class CameraScript : MonoBehaviour {
 		}
 		else
 		{
-			v = new Vector3 (playerObj.transform.position.x, 33 + ((y-20)/4), playerObj.transform.position.z -5 );
+			v = new Vector3 (x_pos + playerObj.transform.position.x, 33 + ((y-20)/4), z_pos + playerObj.transform.position.z);
 			//v = new Vector3 (playerObj.transform.position.x,  y, playerObj.transform.position.z -5 );
 			transform.position = v;
 			transform.LookAt (playerObj.transform.position);
