a6f91bb88ada41a11192dd3ec1af838716e4e285
NightRise214 <jumpevo.1001@gmail.com>
2015-03-12 14:25:58 -0700

    menu

--- Assets/MenuCam_script.cs
+++ Assets/MenuCam_script.cs
@@ -11,6 +11,7 @@ public class MenuCam_script : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.RotateAround (Vector3.zero, Vector3.up, 1 * Time.deltaTime);
+		transform.RotateAround (Vector3.zero, Vector3.left, 1 * Time.deltaTime);
 		transform.LookAt (Vector3.zero);
 
 	}
