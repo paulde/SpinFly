35d3119917cd7ea22690646481ad2cfb4feb4c1f
Steven Em <steven.em.is@gmail.com>
2015-03-12 07:20:00 -0700

    Redid the start menu and added settings options to menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	private Animator _animator;
+	private CanvasGroup _canvasGroup;
+
+	public bool isOpen {
+		get { return _animator.GetBool("isOpen");}
+		set { _animator.SetBool ("isOpen", value); }
+	}
+
+	public void Awake() {
+		_animator = GetComponent<Animator> ();
+		_canvasGroup = GetComponent<CanvasGroup> ();
+
+		var rect = GetComponent<RectTransform> ();
+		rect.offsetMax = rect.offsetMin = new Vector2 (0, 0);
+	}
+
+	public void Update() {
+		if (!_animator.GetCurrentAnimatorStateInfo (0).IsName ("Open")) {
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = false;
+		} else {
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = true;
+		}
+	}
+}
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+	public Menu CurrentMenu;
+	public void Start() {
+		ShowMenu (CurrentMenu);
+	}
+
+	public void ShowMenu(Menu menu) {
+		if (CurrentMenu != null)
+			CurrentMenu.isOpen = false;
+
+		CurrentMenu = menu;
+		CurrentMenu.isOpen = true;
+	}
+
+	public void StartGame() {
+		Application.LoadLevel(1);
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+}
