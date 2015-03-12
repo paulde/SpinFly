67e929703c3e10864d3b5463d60b8d6c2c8cd77f
NightRise214 <jumpevo.1001@gmail.com>
2015-03-12 14:17:08 -0700

    menu

---
+++ Assets/MenuCam_script.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuCam_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (Vector3.zero, Vector3.up, 1 * Time.deltaTime);
+		transform.LookAt (Vector3.zero);
+
+	}
+}
